2ebf81901d794be6f5c47fd3dc28937188d9a884
Nui-backend-intern <100454921+Mrpk2109@users.noreply.github.com>
2023-11-03 11:24:29 +0700

    feat: products api

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,13 +3,17 @@ import { Module } from '@nestjs/common';
 import { ProductService } from './product/product.service';
 import { ProductController } from './product/product.controller';
 import { ProductModule } from './product/product.module';
+import { CatController } from './cat/cat.controller';
+import { CatService } from './cat/cat.service';
+import { CatModule } from './cat/cat.module';
 
 @Module({
   imports: [
     ProductModule,
+    CatModule,
   ],
-  providers: [ProductService],
-  controllers: [ProductController],
+  providers: [ProductService, CatService],
+  controllers: [ProductController, CatController],
    
 })
 export class AppModule {}
