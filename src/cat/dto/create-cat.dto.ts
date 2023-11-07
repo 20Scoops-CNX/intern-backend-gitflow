d5950b4601ae2ddcd9eef60583d6c8f6309c8b4c
Jhutatip <104407500+Jhutatip@users.noreply.github.com>
2023-11-07 11:05:07 +0700

    feat: Add swagger for cat api

--- src/cat/dto/create-cat.dto.ts
+++ src/cat/dto/create-cat.dto.ts
@@ -1,6 +1,13 @@
 /* eslint-disable prettier/prettier */
+import { ApiProperty } from '@nestjs/swagger';
+
 export class CreateCatDto {
+  @ApiProperty({ type: String, description: 'name' })
   name: string;
+
+  @ApiProperty({ type: String, description: 'age' })
   age: string;
+
+  @ApiProperty({ type: String, description: 'breed' })
   breed: string;
 }
