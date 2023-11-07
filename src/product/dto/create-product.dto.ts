7a972eccf0eda941ae47a63e7b18c371e604bc0a
Nui-backend-intern <100454921+Mrpk2109@users.noreply.github.com>
2023-11-07 10:25:31 +0700

    feat: crud product

---
+++ src/product/dto/create-product.dto.ts
@@ -0,0 +1,6 @@
+export class CreateProductDto{
+    productName: string;
+    spec:string;
+    description: string;
+    productPrice: number;
+}
---
+++ src/product/product.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@nestjs/common';
+import { Product } from './product.schema';
+import { InjectConnection, InjectModel } from '@nestjs/mongoose';
+import { Connection, Model } from 'mongoose';
+import { CreateProductDto } from './dto/create-product.dto';
+import { UpdateProductDto } from './dto/update-product.dto';
+
+@Injectable()
+export class ProductService {
+    constructor (@InjectModel(Product.name) private productModel: Model<Product>) {}
+    
+    async create(createProductDto: CreateProductDto):Promise<Product>{
+        const createProduct = new this.productModel(createProductDto);
+        return createProduct.save();
+    }
+
+    async findAllProducts(): Promise<Product[]> {
+        return this.productModel.find().exec();
+    }
+
+    async update(
+        id: string,
+        updateProduct: UpdateProductDto
+    ): Promise<Product>{
+        return this.productModel.findByIdAndUpdate(id,updateProduct,{
+            new: true,
+        })
+    }
+
+    async delete(id: string): Promise<void>{
+        await this.productModel.findByIdAndRemove(id);
+    }
+}
